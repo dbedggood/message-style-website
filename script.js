4e7c1cbc70811acb6d36703bf39d14777981a25a
dbedggood <ddbedggood@gmail.com>
2019-05-01 16:36:19 +1200

    Reformat messageTimeline in script.js

--- script.js
+++ script.js
@@ -1,64 +1,57 @@
 "use strict";
 
-var messageTimeline = anime.timeline();
+var messageTimeline = anime.timeline({
+  easing: 'easeOutExpo',
+  duration: 1000
+});
 
 messageTimeline
   .add({
     targets: '.messages #message1',
     translateX: 1000,
-    easing: 'easeOutExpo',
-    offset: '+=800'
+    offset: '+=300'
   })
   .add({
     targets: '.messages #message2',
     translateX: 1000,
-    easing: 'easeOutExpo',
-    offset: '+=800'
+    offset: '+=300'
   })
   .add({
     targets: '.messages #message3',
     translateX: 1000,
-    easing: 'easeOutExpo',
-    offset: '+=1500'
+    offset: '+=1000'
   })
   .add({
     targets: '.messages #message4',
     translateX: 1000,
-    easing: 'easeOutExpo',
-    offset: '+=2000'
+    offset: '+=1000'
   })
   .add({
     targets: '.messages #message5',
-    translateX: 1000,
-    easing: 'easeOutExpo',
-    offset: '+=500'
+    translateX: 1000
   })
   .add({
     targets: '.messages #message6',
     translateX: 1000,
-    easing: 'easeOutExpo',
-    offset: '+=1000'
-  })
-  .add({
-    targets: '.messages #message7',
-    translateX: 1000,
-    easing: 'easeOutExpo',
     offset: '+=500'
   })
   .add({
+    targets: '.messages #message7',
+    translateX: 1000
+  })
+  .add({
     targets: '.messages #message8',
-    translateX: 1000,
-    easing: 'easeOutExpo'
+    translateX: 1000
   })
   .add({
     targets: '.cvButton button',
     scale: [
       { value: 0, duration: 300 },
-      { value: 1, duration: 1000 },
+      { value: 1, duration: 800 },
     ],
     opacity: [
       { value: 0, duration: 300 },
-      { value: 1, duration: 1000 }
+      { value: 1, duration: 800 }
     ],
     elasticity: 200
-  });;
+  });
