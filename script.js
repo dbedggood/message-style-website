46a4ef527e6d9f9756ce9720dccc5633691776cd
Daniel Bedggood <ddbedggood@gmail.com>
2019-07-25 13:52:35 +1200

    Update messages

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-"use strict";
+'use strict'
 
 var messageTimeline = anime.timeline({
   easing: 'easeOutExpo',
   duration: 1000
-});
+})
 
 messageTimeline
   .add({
     targets: '.messages #message1',
-    translateX: 1000,
+    translateX: 5000
   })
   .add({
     targets: '.messages #message2',
-    translateX: 1000,
+    translateX: 5000,
     offset: '+=200'
   })
   .add({
     targets: '.messages #message3',
-    translateX: 1000,
+    translateX: 5000,
     offset: '+=600'
   })
   .add({
     targets: '.messages #message4',
-    translateX: 1000,
+    translateX: 5000,
     offset: '+=600'
   })
   .add({
     targets: '.messages #message5',
-    translateX: 1000
+    translateX: 5000
   })
   .add({
     targets: '.messages #message6',
-    translateX: 1000,
+    translateX: 5000,
     offset: '+=400'
   })
   .add({
     targets: '.messages #message7',
-    translateX: 1000
+    translateX: 5000
   })
   .add({
     targets: '.messages #message8',
-    translateX: 1000
+    translateX: 5000
   })
